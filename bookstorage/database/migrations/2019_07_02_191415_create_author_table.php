81835681cf447282f8e909a4b3bf4ab710348473
NakatoTeshika <aleksa_aleksa98@mail.ru>
2019-07-03 02:26:14 +0600

    Файл для миграции

---
+++ bookstorage/database/migrations/2019_07_02_191415_create_author_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Name');
+            $table->string('Surname');
+            $table->string('Patronymic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author');
+    }
+}
